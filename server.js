38699a045abe13a384069ca05bf7cfbea2be83ba
git practice <abhijith7133@gmail.com>
2025-10-28 20:12:17 +0530

    add patch in backend

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],   
   })
 );
